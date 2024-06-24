ba0571e579a46c6d01a522957c2b663e790a5528
Millroy094 <58091953+Millroy094@users.noreply.github.com>
2024-06-24 18:45:27 +0100

    feat: added some ui unit tests

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,18 +1,43 @@
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  DefaultOptions,
+} from '@apollo/client';
 
-import { Home } from "./pages/Home/Home";
-import MoreInfo from "./pages/MoreInfo/MoreInfo";
+import { Home } from './pages/Home/Home';
+import MoreInfo from './pages/MoreInfo/MoreInfo';
+
+const defaultOptions: DefaultOptions = {
+  watchQuery: {
+    fetchPolicy: 'no-cache',
+    errorPolicy: 'ignore',
+  },
+  query: {
+    fetchPolicy: 'no-cache',
+    errorPolicy: 'all',
+  },
+};
+
+const client = new ApolloClient({
+  uri: process.env.REACT_APP_GRAPHQL_API_URL,
+  cache: new InMemoryCache(),
+  defaultOptions: defaultOptions,
+});
 
 const App = () => {
   return (
-    <div className="container">
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/recipe/:id" element={<MoreInfo />} />
-        </Routes>
-      </Router>
-    </div>
+    <ApolloProvider client={client}>
+      <div className='container'>
+        <Router>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/recipe/:id' element={<MoreInfo />} />
+          </Routes>
+        </Router>
+      </div>
+    </ApolloProvider>
   );
 };
 
--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -6,37 +6,12 @@ import '@fontsource/roboto/700.css';
 import App from './App';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  DefaultOptions,
-} from '@apollo/client';
-
-const defaultOptions: DefaultOptions = {
-  watchQuery: {
-    fetchPolicy: 'no-cache',
-    errorPolicy: 'ignore',
-  },
-  query: {
-    fetchPolicy: 'no-cache',
-    errorPolicy: 'all',
-  },
-};
-
-const client = new ApolloClient({
-  uri: process.env.REACT_APP_GRAPHQL_API_URL,
-  cache: new InMemoryCache(),
-  defaultOptions: defaultOptions,
-});
 
 const container = document.getElementById('root')!;
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <ApolloProvider client={client}>
-      <App />
-    </ApolloProvider>
+    <App />
   </React.StrictMode>,
 );
